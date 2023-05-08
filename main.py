796a48ae884b1abad7eaa2a5a0a25580c5da370a
tymoteuszv <tymekvogt@gmail.com>
2023-05-08 19:04:52 +0200

    Added type hints and docstrings.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import randrange
 
 
 class Maze:
-    def __init__(self, tiles=None):
+    def __init__(self, tiles: list[list[int]] = None):
         self.tiles = tiles
         self.size_x = len(self.tiles[0])
         self.size_y = len(self.tiles)
@@ -12,12 +12,12 @@ class Maze:
         self.path_support = set()
 
     def __str__(self):
-        def paint(tile):
-            return ('  ', '\u2588\u2588')[tile]
+        def paint(tile: int) -> str:
+            return ('  ', '\u2588\u2588', '//')[tile]
         return '\n'.join([''.join(tuple(map(paint, row))) for row in self.tiles])
 
     @classmethod
-    def create_maze(cls, size_x, size_y):
+    def create_maze(cls, size_x: int, size_y: int):
         """
         :param size_x: width of the maze grid
         :param size_y: height of the maze grid
@@ -29,7 +29,7 @@ class Maze:
         return instance
 
     @classmethod
-    def from_file(cls, filename):
+    def from_file(cls, filename: str):
         """
 
         :param filename: filename that refers to txt file
@@ -40,12 +40,17 @@ class Maze:
         instance.find_entrances()
         return instance
 
-    def save_to_file(self, filename):
+    def save_to_file(self, filename: str):
         with open(f"{filename}.txt", "w") as file:
             for line in self.tiles:
                 file.write("".join(map(str, line)) + "\n")
 
     def create_entrances(self):
+        """
+        Creates exactly two entrances, in respectively first and last column, i.e. left and right side of the Maze.
+        Both entrances have random position that cannot be a corner. From this position a "tunnel" is then carved until
+        it encounters any tile that is not a wall.
+        """
         self.entrances = ((randrange(1, self.size_y - 1, 2), 0), (randrange(1, self.size_y - 1, 2), self.size_x - 1))
         for y, x in self.entrances:
             self.tiles[y][x] = 0
@@ -65,6 +70,9 @@ class Maze:
                         break
 
     def find_entrances(self):
+        """
+        Finds entrances in a maze and stores them in variable that can be later used by solve_maze
+        """
         entrances = []
         for i, row in enumerate(self.tiles):
             if row[0] == 0:
@@ -75,6 +83,10 @@ class Maze:
         self.entrances = entrances
 
     def generate(self):
+        """
+        Carves random paths inside the Maze grid using Prim's algorithm for Minimum Spanning Tree.
+        Between any two points of the created Maze there is exactly one path.
+        """
         rand_y, rand_x = (randrange(1, self.size_x - 1, 2), randrange(1, self.size_y - 1, 2))
         nodes = set()
         nodes.add((rand_y, rand_x, rand_y, rand_x))
@@ -85,7 +97,13 @@ class Maze:
                 self.tiles[y1][x1] = 0
                 nodes.update(self.find_nodes((y, x)))
 
-    def find_nodes(self, node, solve=False):
+    def find_nodes(self, node: tuple[int, ...], solve: bool = False) -> set[tuple[int, ...]]:
+        """
+        Finds surrounding tiles that meet the requirements and returns them
+        :param node: node of which surrounding will be examined
+        :param solve: when False, functions looks for surrounding wall tiles, otherwise it looks for surrounding
+        path tiles.
+        """
         nodes = set()
         if solve:
             axis = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -94,15 +112,23 @@ class Maze:
             axis = [(-2, 0), (2, 0), (0, -2), (0, 2)]
             offset = 1
         for i, j in axis:
-            if 0 < node[0] + i < self.size_y - 1 and 0 < node[1] + j < self.size_x - offset and \
-                    self.tiles[node[0] + i][node[1] + j] == offset:  # todo
+            if (0 < node[0] + i < self.size_y - 1 and
+                    0 < node[1] + j < self.size_x - offset and
+                    self.tiles[node[0] + i][node[1] + j] == offset):
                 if solve:
                     nodes.add((node[0] + i, node[1] + j))
                 else:
                     nodes.add((node[0] + i, node[1] + j, node[0] + i // 2, node[1] + j // 2))
         return nodes
 
-    def solve_maze(self, node=None, path=None):
+    def solve_maze(self, node: tuple[int, ...] = None, path: list[tuple[int]] = None):
+        """
+        Solves maze by checking every possible path from the entrance until it finds another entrance.
+        All concurrent function instances are stopped when entrance is found.
+        :param node: node where function starts looking for possible path
+        :param path: current path of function instance, it avoids searching repetitions
+        :return:
+        """
         self.path_support.add(node)
         if node is None:
             node = self.entrances[0]
@@ -117,14 +143,13 @@ class Maze:
             self.solve_maze(new_node, path + [node])
 
     def printable_solved_maze(self):
+        """
+        Invokes solve_maze if Maze has not been already solved. Adds solution path to Maze grid.
+        """
         if not self.solved:
             self.solve_maze()
         for y, x in self.path:
             self.tiles[y][x] = 2
-
-        def paint(tile):
-            return ('  ', '\u2588\u2588', '//')[tile]
-        return '\n'.join([''.join(tuple(map(paint, row))) for row in self.tiles])
 
 
 class Menu:
@@ -166,7 +191,8 @@ class Menu:
             elif command == "4":
                 print(self.maze)
             elif command == "5":
-                print(self.maze.printable_solved_maze())
+                self.maze.printable_solved_maze()
+                print(self.maze)
 
 
 if __name__ == "__main__":
